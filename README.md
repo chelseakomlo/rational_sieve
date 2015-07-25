ddff70ff57bac5aad9fa7a8f50818a64fc1f6e93
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2015-07-24 21:59:44 -0500

    initial commit, incorrect generation of z

---
+++ README.md
@@ -0,0 +1,7 @@
+## Rational Sieve
+
+In mathematics, the rational sieve is a general algorithm for factoring integers into prime factors. 
+
+While it is rather useless as a practical factoring algorithm, it is a helpful first step for those trying to understand how the general number field sieve works.
+
+source1: https://en.wikipedia.org/wiki/Rational_sieve
---
+++ sieve.py
@@ -0,0 +1,32 @@
+
+# lazy, you could use the sieve of Eratosthenes to find primes
+PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 
+          127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181]
+n = 187
+bound = 7 # chosen aribitrarily
+factor_base = [2, 3, 5, 7] # primes less than chosen bound
+
+# first step is to test for divisibility by each number in the factor base. 
+# if n is evenly divided by one of these, then we are finished.
+
+# find positive integers z, where z is a number whose prime factors are in the factor_base
+
+z = []
+# go through and calculate z numbers whom all of their prime factors are in the factor_base
+
+for i in range(2, n):
+  given = i
+  factors = []
+  for prime in PRIMES:
+    if given % prime == 0:
+      factors.append(prime)
+      given //= prime
+
+  print "factors %s" % factors
+  not_in_z = [factor for factor in factors if factor not in factor_base]
+  if (len(not_in_z) == 0): z.append(i) 
+  print "counter %s" % i
+
+print "z %s" % z
+
+
