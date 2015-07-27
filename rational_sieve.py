e319ea48d78e61f204dd50d3ae9965f2d36c64c7
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2015-07-26 20:10:31 -0500

    find b_smooth numbers

--- rational_sieve.py
+++ rational_sieve.py
@@ -1,18 +1,27 @@
-n = 187
-bound = 11
-factor_base = [2, 3, 5, 7, 11]
-
-
 
 class Sieve():
 
+  def __init__(self, n, bound, factor_base):
+    self.n = n
+    self.bound = bound
+    self.factor_base = factor_base
+
   def pfactor(self, given):
     pfactors = []
-    for factor in factor_base:
-      while given % factor == 0:
-        pfactors.append(factor)
-        given //= factor
-    if given > 0: pfactors.append(given)
-
+    for i in range(2, given):
+      while given % i == 0:
+        pfactors.append(i)
+        given //= i
+    if given > 1: pfactors.append(given)
     return pfactors
 
+  def build_b_smooth(self): 
+    self.relations = []
+    for i in range(2, self.n): 
+      if self._b_smooth(i): 
+        relation = {"z": self.pfactor(i), "zn": self.pfactor(i+self.n)}
+        self.relations.append(relation)
+
+  def _b_smooth(self, given):
+   factors = self.pfactor(given) + self.pfactor(given + self.n) 
+   return max(factors) in self.factor_base
--- test_sieve.py
+++ test_sieve.py
@@ -5,7 +5,10 @@ from rational_sieve import Sieve
 class TestRationalSieve():
 
   def setUp(self):
-    self.sieve = Sieve()
+    self.n = 55
+    self.bound = 11
+    self.factor_base = [2, 3, 5, 7, 11]
+    self.sieve = Sieve(self.n, self.bound, self.factor_base)
 
   def test_rational_sieve_finds_prime_factors_for_non_prime(self):
     prime_factors = self.sieve.pfactor(9)
@@ -14,3 +17,10 @@ class TestRationalSieve():
   def test_rational_sieve_finds_prime_factors_for_prime(self):
     prime_factors = self.sieve.pfactor(17)
     assert_equals(1, prime_factors.count(17))
+
+  def test_rational_sieve_builds_a_list_of_b_smooth_numbers(self):
+    self.sieve.build_b_smooth()
+    first = self.sieve.relations[0]
+    max_pfactor = max(first["z"] + first["zn"])
+    assert_true(max_pfactor in self.factor_base)
+    
