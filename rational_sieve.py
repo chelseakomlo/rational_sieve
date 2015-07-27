4123dd48b6b75560a3ca071af65802731c28ad13
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2015-07-26 21:06:07 -0500

    adding cache

--- rational_sieve.py
+++ rational_sieve.py
@@ -5,21 +5,13 @@ class Sieve():
     self.n = n
     self.bound = bound
     self.factor_base = factor_base
-
-  def pfactor(self, given):
-    pfactors = []
-    for i in range(2, given):
-      while given % i == 0:
-        pfactors.append(i)
-        given //= i
-    if given > 1: pfactors.append(given)
-    return pfactors
+    self.cache = {}
 
   def build_b_smooth(self): 
     self.relations = []
     for i in range(2, self.n): 
       if self._b_smooth(i): 
-        relation = {"z": self.pfactor(i), "zn": self.pfactor(i+self.n)}
+        relation = {"z": self.pfactors(i), "zn": self.pfactors(i+self.n)}
         self.relations.append(relation)
 
   def even(self, factors):
@@ -27,7 +19,21 @@ class Sieve():
       if (factors.count(unique) % 2) != 0: return False
     return True
 
+  def pfactors(self, given):
+    if self.cache.get(given): return self.cache[given]
+    return self._build_pfactors(given)
+
+  def _build_pfactors(self, given):
+    pfactors = []
+    for i in range(2, given):
+      while given % i == 0:
+        pfactors.append(i)
+        given //= i
+    if given > 1: pfactors.append(given)
+    self.cache[given] = pfactors
+    return pfactors
+
   def _b_smooth(self, given):
-   factors = self.pfactor(given) + self.pfactor(given + self.n) 
+   factors = self.pfactors(given) + self.pfactors(given + self.n) 
    return max(factors) in self.factor_base
 
