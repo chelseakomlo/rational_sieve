13da7d4d82977c1666c48fd8364531959e7cf5f7
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2015-07-27 23:04:52 -0500

    removing cache

--- rational_sieve.py
+++ rational_sieve.py
@@ -7,7 +7,6 @@ class Sieve():
     self.n = n
     self.bound = bound
     self.factor_base = factor_base
-    self.cache = {}
 
   def get_factors(self):
     relations = self.build_b_smooth()
@@ -20,9 +19,10 @@ class Sieve():
   def build_b_smooth(self): 
     relations = []
     for i in range(2, self.n): 
-      if self._b_smooth(i): 
-        relation = {"z": self.pfactors(i), "zn": self.pfactors(i+self.n)}
-        relations.append(relation)
+      z_factors = self.pfactors(i)
+      if self._b_smooth(z_factors): 
+        zn_factors = self.pfactors(i+self.n)
+        relations.append({"z": z_factors, "zn": zn_factors})
     return relations
 
   def even(self, factors):
@@ -30,28 +30,22 @@ class Sieve():
       if (factors.count(unique) % 2) != 0: return False
     return True
 
-  def pfactors(self, given):
-    if self.cache.get(given): return self.cache[given]
-    return self._build_pfactors(given)
-
   def get_gcd(self, relation):
     z_pfactrs = reduce(lambda x, y: x*y, set(relation["z"])) 
     zn_pfactrs = reduce(lambda x, y: x*y, set(relation["zn"])) 
     return (gcd((zn_pfactrs - z_pfactrs), self.n), gcd((zn_pfactrs + z_pfactrs), self.n))
 
-  def _build_pfactors(self, given):
+  def pfactors(self, given):
     pfactors = []
     for i in range(2, given):
       while given % i == 0:
         pfactors.append(i)
         given //= i
     if given > 1: pfactors.append(given)
-    self.cache[given] = pfactors
     return pfactors
 
-  def _b_smooth(self, given):
-   factors = self.pfactors(given) + self.pfactors(given + self.n) 
-   return max(factors) in self.factor_base
+  def _b_smooth(self, pfactr):
+   return max(pfactr) in self.factor_base
 
   def _is_factorization(self, gcd):
     return gcd[0] * gcd[1] == self.n
